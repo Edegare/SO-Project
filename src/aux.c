f7914e72d5b7613ca63a1766083ec7c7dd434378
Edgar Ferreira <edgarcferreira03@gmail.com>
2024-04-30 02:47:04 +0100

    Warnings correction: glib

--- src/aux.c
+++ src/aux.c
@@ -5,10 +5,18 @@
 
 #include "defines.h"
 
-gboolean find_msg(gpointer element, gpointer pid) {
-    Msg msg = (Msg) element;
-    int target_pid = GPOINTER_TO_INT(pid);
-    return (msg->pid == target_pid);
+gboolean find_msg_remove(GArray *array, int pid) {
+    guint index;
+    for (index = 0; index < array->len; index++) {
+        Msg msg = g_array_index(array, Msg, index);
+        if (msg->pid == pid) {
+            free(msg);
+            g_array_remove_index(array, index);
+
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
 
 
